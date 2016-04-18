fa014777078aee97da7776d9c364d12743666e82
Luca Zimmermann <git@sndstrm.de>
2016-04-18 15:55:34 +0200

    Added debug mode, added time output, added http timeout handling

--- worker.py
+++ worker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 
-import urllib, subprocess, os, string, json, re
+import urllib, urllib2, subprocess, os, string, json, re, sys
 from datetime import date, datetime, time, timedelta
 from sunrise import sun
 from time import sleep
@@ -14,11 +14,14 @@ stationBoardTo=time(hour=8,minute=0,second=0)
 
 s = sun(lat=52.37,long=9.72)
 lightIs = False
-doorState = False
 black = (0,0,0)
 yellow = (255,255,0)
 
+debug = False
+if len(sys.argv) >= 2:
+	debug = (sys.argv[1] == "debug")
 
+bigfont = ImageFont.truetype("5x7.ttf", 16)
 digfont = ImageFont.truetype("4x7.ttf", 7)
 font =  ImageFont.truetype("5x7.ttf", 8)
 
@@ -28,32 +31,49 @@ def isDarkOutside():
 def URLRequest(url, params, method="GET"):
 	try:
 		params = urllib.urlencode(params)
-		if method == "POST":
-			f = urllib.urlopen(url, params)
-		else:
+		if debug:
 			print(url+'?'+params)
-			f = urllib.urlopen(url+'?'+params)
+		if method == "POST":
+			f = urllib2.urlopen(url, params, timeout=10)
+		else:
+			f = urllib2.urlopen(url+'?'+params, None, timeout=10)
+		if debug:
 			print("done.")
 		return f.read()
-	except Exception:
+	except Exception, e:
+		if debug:
+			print("Exception while fetching: ",e)
 		return False
 
 def doorIsOpen():
 	return URLRequest("http://leinelab.net/ampel/status.txt", {}) == "OpenLab"
 
+def getTimeImage():
+	now = datetime.now()
+	img = Image.new('RGB', (width, height))
+	draw = ImageDraw.Draw(img)
+	tstr = now.strftime("%H:%M:%S")
+	for i in range(0,8):
+		w, h = bigfont.getsize(tstr[i])
+		draw.text((16+i*10+(10-w)/2, 1), tstr[i], yellow, font=bigfont)
+	if debug:
+		img.save('sample-out.png')
+	return img
+	
+
 def getStationImage(stationId):
 	now = datetime.now()
 	req_for = (now + timedelta(0,60*5))
-	depatures = json.loads(URLRequest("http://pebble.sndstrm.de/fahrplan/stationBoard.php", {"ext_id":stationId,"max":10,"date":req_for.strftime("%d.%m.%y"),"time":req_for.strftime("%H:%M")}))
+	req = URLRequest("http://pebble.sndstrm.de/fahrplan/stationBoard.php", {"ext_id":stationId,"max":10,"date":req_for.strftime("%d.%m.%y"),"time":req_for.strftime("%H:%M")})
+	if req == False:
+		return False
+	depatures = json.loads(req)
 	station = {"in":list(), "out":list()}
 	for depature in depatures["Journey"]:
 		depOb = {}
 		dep = depature["attributes"]
 		dest = dep["targetLoc"]
 		d_time = datetime.strptime(dep["fpDate"]+" "+dep["fpTime"], '%d.%m.%y %H:%M')
-		#print(d_time)
-		#print(" --- ")
-		#print(d_time-now)
 		depOb["line"] = re.search('[0-9]+',dep["prod"]).group(0)
 		depOb["in"] = int((d_time-now).total_seconds()/60)
 		if re.search('Aegidientorplatz', dest):
@@ -98,8 +118,8 @@ def getStationImage(stationId):
 		else:
 			depOb["destination"] = dest[:6]
 			station["out"].append(depOb)
-	#debug
-	#print station
+	if debug:
+		print station
 	img = Image.new('RGB', (width, height))
 	draw = ImageDraw.Draw(img)
 	draw.rectangle(((0,0),(12,16)), fill=yellow)
@@ -123,20 +143,20 @@ def getStationImage(stationId):
 	draw.rectangle(((103,0),(112,16)), fill=yellow)
 	draw.text((47+57, 1), str(station["out"][0]["in"]).rjust(2, "0"), black, font=digfont)
 	draw.text((47+57, 9), str(station["out"][1]["in"]).rjust(2, "0"), black, font=digfont)
-	#DEBUG
-	img.save('sample-out.png')
+	if debug:
+		img.save('sample-out.png')
 	return img
 
 def pushImage(img):
-	bitmap = ""
-	for pixel in list(img.getdata()):
-		bitmap += "0" if pixel == black else "1"
-	URLRequest('http://'+ip+'/api', {'drawBitmap':bitmap})
+	if img != False:
+		bitmap = ""
+		for pixel in list(img.getdata()):
+			bitmap += "0" if pixel == black else "1"
+		URLRequest('http://'+ip+'/api', {'drawBitmap':bitmap})
 
-
-
-doorState = not doorIsOpen()
-getStationImage("000638934")
+if debug:
+	getStationImage("000638934")
+	getTimeImage()
 
 while True:
 	if lightIs != isDarkOutside():
@@ -148,9 +168,14 @@ while True:
 	):
 		for i in range[0:5]:
 			if i == 0:
-				for s in range[0:10]:
-					#push some bitmap with current time
-					sleep(1)
+				show_for = datetime.now()+timedelta(0,10)
+				while datetime.now() <= show_for:
+					st = datetime.now()
+					pushImage(getTimeImage())
+					try:
+						sleep(1-(datetime.now()-st).total_seconds())
+					except Exception:
+						pass
 			elif i == 1:
 				URLRequest('http://'+ip+'/api', {'fillScreen':0,'setCursor':1,'x':0,'y':0,'print':"Abfahrten von\nGoetheplatz"})
 				sleep(5)
@@ -163,9 +188,14 @@ while True:
 			elif i == 4:
 				pushImage(getStationImage("000638551"))
 				sleep(15)
-		doorState = not doorIsOpen()
 	else:
-		if doorState != doorIsOpen():
-			URLRequest('http://'+ip+'/api', {'fillScreen':0,'setCursor':1,'x':0,'y':0,'print':"LeineLab ist\n"+("offen" if doorState else "geschlossen")})
-			doorState = not doorState
-		sleep(1)
+		URLRequest('http://'+ip+'/api', {'fillScreen':0,'setCursor':1,'x':0,'y':0,'print':"LeineLab ist\n"+("offen" if doorIsOpen() else "geschlossen")})
+		sleep(50)
+		show_for = datetime.now()+timedelta(0,10)
+		while datetime.now() <= show_for:
+			st = datetime.now()
+			pushImage(getTimeImage())
+			try:
+				sleep(1-(datetime.now()-st).total_seconds())
+			except Exception:
+				pass
